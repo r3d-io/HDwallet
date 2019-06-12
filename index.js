8fa22100b940ff4892370aec143d7a171de08f29
Shanu goyanka <shanu@techracers.io>
2019-06-12 13:43:06 +0530

    Used Hd key library for masterkey generation

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const bip39 = require('bip39')
+const hdkey = require('hdkey')
+
+const mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+const root = hdkey.fromMasterSeed(seed);
+const masterPrivateKey = root._privateKey.toString('hex');
+const masterPublicKey = root._publicKey.toString('hex');
+
+console.log(mnemonic + "\n" + seed.toString('hex') + "\n" + masterPrivateKey + "\n" + masterPublicKey )
+
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 })
-
-const mnemonic = bip39.generateMnemonic()
-const seed = bip39.mnemonicToSeedSync(mnemonic)
-console.log(seed)
 readline.question('Enter seed length 12 15 18 21 24', (seedlength) => {
-
+  
   readline.close()
 })
